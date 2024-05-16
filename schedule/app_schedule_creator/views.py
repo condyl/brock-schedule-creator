00c2ee0ecb743ff5ca09e29b36a3f7769494dec9
condyl <Condyl678@gmail.com>
2024-05-16 00:22:42 -0400

    add the most incredibly ugly javascript code you have ever witnessed with your own eyes

--- schedule/app_schedule_creator/views.py
+++ schedule/app_schedule_creator/views.py
@@ -20,17 +20,14 @@ def schedule(request):
     #print(courses)
     valid_course_options = get_course_options(courses, semester, 2024)
     context['valid_course_options'] = valid_course_options
-    print(context['valid_course_options'][0][0][0].course_start_time)
-    #for choice in valid_course_options:
-    #    print(choice)
-    #    for courses in choice:
-    #        for course in courses:
-    #            #print(course.course_name, course.course_type, course.course_days, course.course_start_time, course.course_end_time, course.course_instructor)
-    #            pass
-    #    print()
-    
-    #print(course_options)
-
+    #print(context['valid_course_options'][0][0][0].course_start_time)
+    for choice in valid_course_options:
+        print(choice)
+        for courses in choice:
+            for course in courses:
+                print(course.course_name, course.course_type, course.course_days, course.course_start_time, course.course_end_time, course.course_instructor)
+                pass
+        print()
     return render(request, 'schedule.html', context=context)
 
 def upload(request):
